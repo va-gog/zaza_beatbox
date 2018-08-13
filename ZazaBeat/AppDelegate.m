cfdad76421113d10bac767583b4d0ec4166bf1d3
Gohar Vardanyan <gv@GVMM.local>
2018-08-13 17:05:31 +0400

    After adding .DS_Store in gitignore

--- ZazaBeat/AppDelegate.m
+++ ZazaBeat/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //barev dzez
     return YES;
 }
 
